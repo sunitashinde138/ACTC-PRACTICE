91bc614b969feadfde3cc9a067e154695b3c2bf0
prati <prati@LAPTOP-KFM1E4C4.bbrouter>
2022-09-03 13:53:50 +0530

    my First logical program

---
+++ src/SAMPLE1/PRATIBHA.java
@@ -0,0 +1,23 @@
+package SAMPLE1;
+
+public class PRATIBHA //LOGICAL PROGRAM
+{
+	public static void main(String[] args) 
+	{
+		
+		 int ar[]={10,3,4,6,7};
+	        int i;
+	        for(i=0;i<5;i++)
+	        {
+	             if(ar[i]%2==0)
+	            { 
+	        
+	        System.out.println(+ar[i]+"is even no"); 
+	            }
+	            else
+	            {
+	            System.out.println(+ar[i]+"is odd no");
+	            }
+	        }
+	    }	
+}
